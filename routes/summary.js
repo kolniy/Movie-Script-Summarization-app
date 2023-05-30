07d7242c79b22aa5432a4b38a586d27816049872
kolawole olaniyi <kolaniyi3@gmail.com>
2023-05-30 16:44:40 +0100

    backend wired to handle summarization of large file, thus bypassing the token limitation

--- routes/summary.js
+++ routes/summary.js
@@ -1,17 +1,44 @@
+import fs from "fs";
+import path from "path";
 import express from "express";
+import { readPdfText } from "pdf-text-reader";
+import multer from "multer";
 import summaryAndEntityExtraction from "../utility/summarizationAndEntityExtraction.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const text = req.body;
-  if (text.length === 0) {
-    return res.json({
-      errors: [{ msg: "text input required" }],
-    });
+/** Multer */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ dest: "uploads/", storage });
+
+router.post("/", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    return res.json({ msg: "file not found" });
   }
+  let fileContents = "";
   try {
-    const entities = await summaryAndEntityExtraction(text);
+    const filePath = req.file.path;
+    const pages = await readPdfText(filePath);
+    pages.forEach((page) => {
+      fileContents += page.lines;
+    });
+
+    // delete uploaded file after reading contents into memory
+    fs.unlink(filePath, (err) => {
+      if (err) throw err;
+    });
+
+    const entities = await summaryAndEntityExtraction(fileContents);
     res.json(entities);
   } catch (error) {
     console.log(error);
--- utility/summarizationAndEntityExtraction.js
+++ utility/summarizationAndEntityExtraction.js
@@ -1,21 +1,33 @@
-import { LLMChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain/prompts";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
 const openAIApiKey = process.env.OPENAPI_APIKEY;
 
 const summarizationAndEntityExtraction = async (text) => {
   const model = new OpenAI({ temperature: 0.9, openAIApiKey: openAIApiKey });
-  const template =
-    " Your task is to summarize the TV script I give you and extract name of all Actors and use bulletpoint to list them. pick good emoji to represent each actor. from this file {file}?";
-  const prompt = new PromptTemplate({
-    template: template,
-    inputVariables: ["file"],
-  });
 
-  const chain = new LLMChain({ llm: model, prompt: prompt });
-  const res = await chain.call({ file: text });
-  return res;
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
+  const docs = await textSplitter.createDocuments([text]);
+
+  const vectorStore = await HNSWLib.fromDocuments(
+    docs,
+    new OpenAIEmbeddings({
+      openAIApiKey: openAIApiKey,
+      modelName: "text-embedding-ada-002",
+    })
+  );
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever()
+  );
+  const question =
+    "Your task is to analyze and summarize the TV script I give you with a title summary and extract the names of all actors with a title actors. Use bullet points to list them and pick a good emoji to represent each actor.";
+  const result = await chain.call({ question, chat_history: [] });
+  return result;
 };
 
 export default summarizationAndEntityExtraction;
